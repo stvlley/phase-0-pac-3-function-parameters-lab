3473c73caf26f7a2620e5743c10c75c026a56ba9
stephen talley <stvlley.amzn@gmail.com>
2022-01-04 16:45:48 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const name = Aki;
+
+function introduction(name) {
+    return(`Hi, my name is ${name}.`);
+}
+const language = JavaScript
+
+function introductionWithLanguage(name, language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+
+
